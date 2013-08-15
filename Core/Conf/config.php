ce4679d863b393393681618ed8c46d7b4651b611
4u4v <admin@4u4v.net>
2013-08-15 17:45:23 +0800

    前台邮件发送模块
    添加前台邮件发送模块。

--- Core/Conf/config.php
+++ Core/Conf/config.php
@@ -36,6 +36,16 @@ $array = array(
 		'ERROR_PAGE'           => './Public/tips/error.html',// 异常和错误
 		
 		'SHOW_PAGE_TRACE'      => true,	 // 显示TRACE页面
+
+		//邮件配置
+		'SMTP_SERVER' =>'smtp.163.com',					//邮件服务器
+		'SMTP_PORT' =>25,								//邮件服务器端口
+		'SMTP_USER_EMAIL' =>'4u4v@163.com', 	//SMTP服务器的用户邮箱(一般发件人也得用这个邮箱)
+		'SMTP_USER'=>'4u4v@163.com',			//SMTP服务器账户名
+		'SMTP_PWD'=>'15083523531',							//SMTP服务器账户密码
+		'SMTP_MAIL_TYPE'=>'HTML',						//发送邮件类型:HTML,TXT(注意都是大写)
+		'SMTP_TIME_OUT'=>60,							//超时时间
+		'SMTP_AUTH'=>true,								//邮箱验证(一般都要开启)
 		
 		//多语言配置
 		'LANG_SWITCH_ON'       => true,   // 开启多语言功能
--- Core/Lib/Action/Home/IndexAction.class.php
+++ Core/Lib/Action/Home/IndexAction.class.php
@@ -28,4 +28,39 @@ class IndexAction extends HomeAction {
 		$this->content = '<p>这里是<a href="http://shuimu.js.cn">广告</a>内容</p>';
 		$this->display();
 	}
+
+    public function mail_form(){
+		$this->title = "邮件发送表单"; // 进行模板变量赋值
+		$this->content = '<p>这里设计一个邮件发送表单。</p>';
+		$this->display();
+	}
+	
+    public function send_mail() {
+   //$config = C('THINK_EMAIL');
+   import('ORG.Util.Email');
+        //此处多余可自行改为Model自动验证
+        if(empty($_POST['mailto'])) {
+            $this->error('收件人Email必须！');
+        }elseif (empty($_POST['subject'])){
+            $this->error('请输入邮件标题！');
+        }elseif (empty($_POST['body'])){
+            $this->error('请输入邮件内容！');
+        }
+	$data['mailto']  =$_POST['mailto'];
+	$data['subject'] =$_POST['subject'];
+	$data['body']    =$_POST['body'];
+   $mail = new Email();
+   if($mail->send($data))
+   {
+		$result = '邮件发送成功!';
+   }
+   else
+   {
+		$result = '邮件发送失败!';
+   } 
+		$this->assign('result',$result);
+		$this->show('<p><br>Mailer<font color="red">{$result}</font></p>');
+	//$mail->debug(true)->send($data);   //开启调试功能
+	}
+
 }
